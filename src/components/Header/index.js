8d7cafbea0e6508af939bbd89afa7e4f53d6b0c5
Niki <niki.patel@asticsinc.com>
2018-12-20 11:21:03 +0530

    spelling change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,23 @@
-import React from "react"
-import "./header.scss"
-import { Layout } from 'antd';
-const { Header } = Layout
+import React from "react";
+import "./header.scss";
+import { Layout } from "antd";
+const { Header } = Layout;
 export default class MyHeader extends React.Component {
-    render() {
-        console.log(this.props)
-        return (
-            <Header className="my-header">
-                <div >
-                    <span onClick={() => {
-                        localStorage.removeItem("token")
-                        this.props.history.push('login')
-                    }}>
-                        Sing Out
-                    </span>
-                </div>
-            </Header>
-        )
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <Header className="my-header">
+        <div>
+          <span
+            onClick={() => {
+              localStorage.removeItem("token");
+              this.props.history.push("login");
+            }}
+          >
+            Sign Out
+          </span>
+        </div>
+      </Header>
+    );
+  }
 }
